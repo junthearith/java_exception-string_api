c1bc0e5f892de890298cfd9398aeeec22d136c9f
junthearith <junthearith@gmail.com>
2024-05-22 10:45:53 +0700

    add view package and View class display menu

---
+++ src/view/View.java
@@ -0,0 +1,14 @@
+package view;
+
+public class View {
+    public static void menu() {
+        System.out.println("=====================================");
+        System.out.println("1. Add new Course");
+        System.out.println("2. List Courses");
+        System.out.println("3. Find Course by ID");
+        System.out.println("4. Find Course by Title");
+        System.out.println("5. Remove Course by ID");
+        System.out.println("0/99. Exit");
+        System.out.println("=====================================");
+    }
+}

d96130ab7b3ad04d3ab11273fd0a6cd91806f5f3
junthearith <junthearith@gmail.com>
2024-05-22 10:49:16 +0700

    add model

---
+++ src/model/Course.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Course {
+    private Integer id;
+    private String title;
+    private String instructor;
+    private String requirement;
+    private String startDate;
+
+    public Course() {
+    }
+
+    public Course(Integer id, String title, String instructor, String requirement, String startDate) {
+        this.id = id;
+        this.title = title;
+        this.instructor = instructor;
+        this.requirement = requirement;
+        this.startDate = startDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getRequirement() {
+        return requirement;
+    }
+
+    public void setRequirement(String requirement) {
+        this.requirement = requirement;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+}
